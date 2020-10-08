177d82de3c9a73eb4dade085ae957f0b118e06a9
majasma <59877756+majasma@users.noreply.github.com>
2020-10-08 10:57:49 +0200

    Files for labday 6

--- source/uart.h
+++ source/uart.h
@@ -1,9 +1,37 @@
-#include "avr/io.h"
+/*
+ * uart.h
+ *
+ * Author: Gustav O. Often and Eivind H. Jølsgard
+ *
+ * For use in TTK4155 Embedded and Industrial Computer Systems Design
+ * NTNU - Norwegian University of Science and Technology
+ *
+ * A simple interface for receiving and transmitting characters to a computer using UART via the on board USB-connector
+ */ 
 
-void uart_init(unsigned int ubrr);
 
-void uart_transmit (unsigned char data);
+#ifndef UART_H_
+#define UART_H_
 
-unsigned char uart_recieve (void);
+#include <stdint.h>
+#define UART_RINGBUFFER_SIZE 64
+/*
+ * Ringbuffer for receiving characters from  
+ */
+typedef struct uart_ringbuffer_t
+{
+	uint8_t head, tail;
+	char data[UART_RINGBUFFER_SIZE];
+	} uart_ringbuffer;
 
-void string_init(unsigned int ubrr);
+
+void configure_uart(void);
+
+int uart_getchar(uint8_t *c);
+int uart_putchar(const uint8_t c);
+
+void UART_Handler       ( void );
+
+
+
+#endif /* UART_H_ */
